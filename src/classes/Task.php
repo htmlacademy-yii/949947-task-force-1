53bad0237c252076c5a140480a1e920f1a6f07eb
Kollc <46793445+Kollc@users.noreply.github.com>
2019-10-30 20:15:27 +0300

    Исправил ошибки (1)

--- src/classes/Task.php
+++ src/classes/Task.php
@@ -1,102 +1,94 @@
 <?php
+
 namespace Academy\classes;
+
 use Exception;
 
-class Task {
-    const STATUS_NEW = 'новое';
-    const STATUS_PROCESSING = 'выполняется';
-    const STATUS_CANCELED = 'отменено';
-    const STATUS_FINISHED = 'завершено';
-    const STATUS_FAILED = 'провалено';
+class Task
+{
+    const STATUS_NEW = 'new';
+    const STATUS_PROCESSING = 'processing';
+    const STATUS_CANCELED = 'canceled';
+    const STATUS_FINISHED = 'finished';
+    const STATUS_FAILED = 'failed';
 
-    const ACTION_NEW = 'новое';
-    const ACTION_FAIL = 'отказ';
-    const ACTION_CANCEL = 'отмена';
-    const ACTION_START = 'старт';
-    const ACTION_FINISH = "завершить";
+    const ACTION_NEW = 'new';
+    const ACTION_FAIL = 'fail';
+    const ACTION_CANCEL = 'cancel';
+    const ACTION_START = 'start';
+    const ACTION_FINISH = "finish";
 
-    const ROLE_EXECUTOR = 'исполнитель';
-    const ROLE_CUSTOMER = 'заказчик';
+    const ROLE_EXECUTOR = 'executor';
+    const ROLE_CUSTOMER = 'customer';
 
-    private $executor_id;
-    private $customer_id;
-    private $status;
+    private $idExecutor;
+    private $idCustomer;
+    private $status = self::STATUS_NEW;
     private $date;
 
-    public function __construct(int $customer_id, int $executor_id)
+    private const RELATIONS = [
+        self::ACTION_NEW => self::STATUS_NEW,
+        self::ACTION_START => self::STATUS_PROCESSING,
+        self::ACTION_FINISH => self::STATUS_FINISHED,
+        self::ACTION_FAIL => self::STATUS_FAILED,
+        self::ACTION_CANCEL => self::STATUS_CANCELED,
+    ];
+
+    public function __construct(int $idCustomer, int $idExecutor)
     {
-        $this->status = self::STATUS_NEW;
-        $this->customer_id = $customer_id;
-        $this->executor_id = $executor_id;
+        $this->idCustomer = $idCustomer;
+        $this->idExecutor = $idExecutor;
     }
 
-    public function getNextStatus (string $action)
+    /**
+     * Возвращает статус в зависимости от действия
+     *
+     * @param string $action
+     *
+     * @return string|null
+     */
+
+    public function getNextStatus(string $action)
     {
-        switch ($action) {
-            case self::ACTION_NEW:
-                return self::STATUS_NEW;
-
-            case self::ACTION_FAIL:
-                return self::STATUS_FAILED;
-                break;
-
-            case self::ACTION_CANCEL:
-                return self::STATUS_CANCELED;
-                break;
-
-            case self::ACTION_START:
-                return self::STATUS_PROCESSING;
-                break;
-
-            case self::ACTION_FINISH:
-                return self::STATUS_FINISHED;
-                break;
-
-            default:
-                return null;
+        if (isset(self::RELATIONS[$action])) {
+            return self::RELATIONS[$action];
         }
+        return null;
     }
 
-    public function cancel (int $initiator_id) {
-        if ($initiator_id === $this->executor_id) {
-            throw new Exception('Отменяющий задачу не является заказчиком');
-        }
-        if ($this->status !== self::STATUS_NEW) {
-            throw new Exception('Отменить задачу можно тогда, когда имеет статус новая');
-        }
-        $this->status = self::STATUS_CANCELED;
-        echo "Статус - отменено";
+    /**
+     * Возвращает список статусов
+     *
+     * @return array
+     */
+    public function getStatusList()
+    {
+        return [
+            self::STATUS_NEW,
+            self::STATUS_PROCESSING,
+            self::STATUS_CANCELED,
+            self::STATUS_FINISHED,
+            self::STATUS_FAILED
+        ];
     }
 
-    public function start (int $initiator_id) {
-        if ($initiator_id === $this->customer_id) {
-            throw new Exception('Взять на выполнение задачу может только исполнитель, а не заказчик');
-        }
-        $this->status = self::STATUS_PROCESSING;
-        echo "Статус - в процессе";
+    /**
+     * Возвращает список действий
+     *
+     * @return array
+     */
+    public function getActionList()
+    {
+        return [self::ACTION_NEW, self::ACTION_FAIL, self::ACTION_CANCEL, self::ACTION_START, self::ACTION_FINISH];
     }
 
-    public function fail (int $initiator_id) {
-        if ($initiator_id === $this->customer_id) {
-            throw new Exception('Отказ от задачи доступен только исполнителю');
-        }
-        if ($this->status === self::STATUS_PROCESSING) {
-            $this->status = self::STATUS_FAILED;
-            echo "Статус - провалено";
-        } else {
-            throw new Exception('Выполнить отказ от задачи можно только при условии, что задача в статусе выполнения');
-        }
-    }
+    /**
+     *
+     *определять список доступных действий, который зависит от: текущего статуса задания,роли пользователя,id пользователя.
+     *
+     */
+    public function availableActions(int $initiator_id)
+    {
 
-    public function finish (int $initiator_id) {
-        if ($initiator_id === $this->executor_id) {
-            throw new Exception('Завершить и принять задачу может только сам заказчик');
-        }
-        if ($this->status === self::STATUS_PROCESSING) {
-            $this->status = self::STATUS_FINISHED;
-            echo "Статус - завершено";
-        } else {
-            throw new Exception('Завершить задачу можно, если она находится в статусе выполнения');
-        }
     }
 }
