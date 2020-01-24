7913464836f0de85912a93645a1e1ad293a9dfe8
Kollc <46793445+Kollc@users.noreply.github.com>
2020-01-24 09:57:57 +0300

    перенес запросы фильтра в БД в модель

--- frontend/controllers/TasksController.php
+++ frontend/controllers/TasksController.php
@@ -33,40 +33,13 @@ class TasksController extends Controller
         $filterModel = new TaskFilter();
 
         $query = new Query();
-        $query->select('*')->from(TaskInfo::tableName() . ' t')->join('JOIN', Categories::tableName() . ' c',
-            't.category_id = c.id');
-        $filterResult = $_POST;
+        $query->select('*')->from(TaskInfo::tableName() . ' t')->join('JOIN',
+            Categories::tableName() . ' c',
+            't.category_id = c.id')->orderBy('dt_add DESC');
 
-//        var_dump($filterResult['TaskFilter']);
-
-        if ($filterModel->load(yii::$app->request->post())) {
-            if (isset($filterResult['TaskFilter']['categories']) and !empty($filterResult['TaskFilter']['categories'])) {
-                foreach ($filterResult['TaskFilter']['categories'] as $categories) {
-                    $query->orWhere(['en_name' => $categories]);
-                }
-            }
-            if ($filterResult['TaskFilter']['withoutExecutor']['0'] === 'withoutExecutor') {
-                $query->andWhere(['executor_id' => null]);
-            }
-
-            if ($filterResult['TaskFilter']['isRemote']['0'] === 'isRemote') {
-                $query->andWhere(['longitude' => null])->andWhere(['latitude' => null]);
-            }
-
-            if (isset($filterResult['TaskFilter']['period'])) {
-                $date = new DateTime();
-                $dateCurrent = new DateTime('now');
-                $date->sub(new DateInterval("P" . $filterResult['TaskFilter']['period'] . "D"));
-                $query->andWhere(['>=', 'dt_add', $date->format('Y-m-d')]);
-                $query->andWhere(['<=', 'dt_add', $dateCurrent->format('Y-m-d')]);
-            }
-
-            if (!empty($filterResult['TaskFilter']['title']) and isset($filterResult['TaskFilter']['title'])) {
-                $query->andWhere(['name' => $filterResult['TaskFilter']['title']]);
-            }
+        if (Yii::$app->request->isPost) {
+            $filterModel->tasksFilter($query, Yii::$app->request->post('TaskFilter'));
         }
-
-        // var_dump($query->prepare(\Yii::$app->db->queryBuilder)->createCommand()->rawSql);
 
         $taskQuery = $query->all();
 
