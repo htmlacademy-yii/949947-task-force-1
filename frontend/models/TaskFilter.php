e38fc8263ee2d9ccef58da0feac18641c26ffdbb
Kollc <46793445+Kollc@users.noreply.github.com>
2020-01-24 09:57:17 +0300

    перенес запросы фильтра в БД в модель

--- frontend/models/TaskFilter.php
+++ frontend/models/TaskFilter.php
@@ -2,7 +2,11 @@
 
 namespace frontend\models;
 
+use DateInterval;
+use DateTime;
+use Yii;
 use yii\base\Model;
+use yii\db\ActiveQuery;
 
 class TaskFilter extends Model
 {
@@ -42,5 +46,33 @@ class TaskFilter extends Model
             self::PERIOD_WEEK => 'Неделя',
             self::PERIOD_MONTH => 'Месяц',
         ];
+    }
+
+    public function tasksFilter($query, $filterResult)
+    {
+        if (isset($filterResult['categories']) and !empty($filterResult['categories'])) {
+            foreach ($filterResult['categories'] as $categories) {
+                $query->orWhere(['en_name' => $categories]);
+            }
+        }
+        if ($filterResult['withoutExecutor']['0'] === 'withoutExecutor') {
+            $query->andWhere(['executor_id' => null]);
+        }
+
+        if ($filterResult['isRemote']['0'] === 'isRemote') {
+            $query->andWhere(['longitude' => null])->andWhere(['latitude' => null]);
+        }
+
+        if (isset($filterResult['period'])) {
+            $date = new DateTime();
+            $dateCurrent = new DateTime('now');
+            $date->sub(new DateInterval("P" . $filterResult['period'] . "D"));
+            $query->andWhere(['>=', 'dt_add', $date->format('Y-m-d')]);
+            $query->andWhere(['<=', 'dt_add', $dateCurrent->format('Y-m-d')]);
+        }
+
+        if (!empty($filterResult['title']) and isset($filterResult['title'])) {
+            $query->andWhere(['name' => $filterResult['title']]);
+        }
     }
 }
